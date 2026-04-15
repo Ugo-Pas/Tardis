42615462d290b8af1c18609de5e27cdc8b194930
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-04-15 13:39:27 +0200

    [+] add dataframe i website

--- tardis_dashboard.py
+++ tardis_dashboard.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
 
+DATASET = "dataset.csv"
+
+raw_df = pd.read_csv(DATASET, on_bad_lines="skip", sep=";")
 st.write("# hello world¡¡")
+
+st.dataframe(raw_df)
