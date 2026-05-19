6e9b535d9b574773db1a38dd05e61db4b002b66e
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-19 09:08:21 +0200

    [~] update: to ruff format

--- src/info_utilisateur.py
+++ src/info_utilisateur.py
@@ -5,8 +5,8 @@
 ## info_utilisateur
 ##
 
-import pandas as pd # panda for open a csv and extract the data for file and data manipulation
-import matplotlib.pyplot as pl # matplotlib to create visualisation via graphs
+import pandas as pd  # panda for open a csv and extract the data for file and data manipulation
+import matplotlib.pyplot as pl  # matplotlib to create visualisation via graphs
 import pydeck as pdk
 import streamlit as st
 import numpy as np
@@ -15,5 +15,7 @@ from src.tools import *
 
 
 def render(df, departure_station, arrival_station, year):
-	st.markdown("<h1 style='text-align: center;'>Info utilisateur</h1>", unsafe_allow_html=True)
-	graph_departure_arrival_station(df, departure_station, arrival_station, year)
+    st.markdown(
+        "<h1 style='text-align: center;'>Info utilisateur</h1>", unsafe_allow_html=True
+    )
+    graph_departure_arrival_station(df, departure_station, arrival_station, year)
